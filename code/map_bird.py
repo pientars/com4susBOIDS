257053d2479c2d6e5e44b6d692bf6b836b40000c
Casey Battaglino <cjbattagl@gmail.com>
2014-04-19 23:42:12 -0400

    sample map code

--- code/map_bird.py
+++ code/map_bird.py
@@ -1,28 +1,41 @@
-from mpl_toolkits import *
-import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap, shiftgrid, cm
 import numpy as np
-# set up orthographic map projection with
-# perspective of satellite looking down at 50N, 100W.
-# use low resolution coastlines.
-map = Basemap(projection='ortho',lat_0=45,lon_0=-100,resolution='l')
-# draw coastlines, country boundaries, fill continents.
-map.drawcoastlines(linewidth=0.25)
-map.drawcountries(linewidth=0.25)
-map.fillcontinents(color='coral',lake_color='aqua')
-# draw the edge of the map projection region (the projection limb)
-map.drawmapboundary(fill_color='aqua')
-# draw lat/lon grid lines every 30 degrees.
-map.drawmeridians(np.arange(0,360,30))
-map.drawparallels(np.arange(-90,90,30))
-# make up some data on a regular lat/lon grid.
-nlats = 73; nlons = 145; delta = 2.*np.pi/(nlons-1)
-lats = (0.5*np.pi-delta*np.indices((nlats,nlons))[0,:,:])
-lons = (delta*np.indices((nlats,nlons))[1,:,:])
-wave = 0.75*(np.sin(2.*lats)**8*np.cos(4.*lons))
-mean = 0.5*np.cos(2.*lats)*((np.sin(2.*lats))**2 + 2.)
-# compute native map projection coordinates of lat/lon grid.
-x, y = map(lons*180./np.pi, lats*180./np.pi)
-# contour data over the map.
-cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
-plt.title('contour lines over filled continent background')
+import matplotlib.pyplot as plt
+
+hsize = 400;
+vsize = 200;
+topoin = np.ones([vsize,hsize]) #etopodata.variables['ROSE'][:]
+lons = np.linspace(20,380,num=hsize);#etopodata.variables['ETOPO05_X'][:]
+lats = np.linspace(-90,90,num=vsize);#etopodata.variables['ETOPO05_Y'][:]
+topoin,lons = shiftgrid(180.,topoin,lons,start=False)
+fig = plt.figure()
+ax = fig.add_axes([0.1,0.1,0.8,0.8])
+
+# ('lcc' = lambert conformal conic).
+# use major and minor sphere radii from WGS84 ellipsoid.
+m = Basemap(llcrnrlon=-145.5,llcrnrlat=1.,urcrnrlon=-2.566,urcrnrlat=46.352,\
+            rsphere=(6378137.00,6356752.3142),\
+            resolution='l',area_thresh=1000.,projection='lcc',\
+            lat_1=50.,lon_0=-107.,ax=ax)
+
+# transform to nx x ny regularly spaced 5km native projection grid
+nx = int((m.xmax-m.xmin)/5000.)+1; ny = int((m.ymax-m.ymin)/5000.)+1
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
+
+# plot image over map with imshow.
+im = m.imshow(topodat,cm.GMT_haxby)
+
+# draw coastlines and boundaries.
+m.drawcoastlines()
+m.drawcountries()
+m.drawstates()
+# draw parallels and meridians.
+# label on left and bottom of map.
+parallels = np.arange(0.,80,20.)
+m.drawparallels(parallels,labels=[1,0,0,1])
+meridians = np.arange(10.,360.,30.)
+m.drawmeridians(meridians,labels=[1,0,0,1])
+# add colorbar
+cb = m.colorbar(im,"right", size="5%", pad='2%')
+ax.set_title('ETOPO5 Topography - Lambert Conformal Conic')
 plt.show()
