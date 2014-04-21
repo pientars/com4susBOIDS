0768dd1024aba6b8eef5655fcc6ed489cb30d5f1
Casey Battaglino <cjbattagl@gmail.com>
2014-04-21 01:58:23 -0400

    plotting colors and lines and shit

--- code/plot_bird.py
+++ code/plot_bird.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+from matplotlib.collections import LineCollection
 import numpy as np
 import csv
+
+def my_color(val):
+    """There're better ways to generate unique colors, but this isn't awful."""
+    return plt.cm.RdBu(val)
 
 data = np.genfromtxt('bird_coords.csv', delimiter=',');
 coords = data[:,3:5].transpose();
@@ -29,12 +34,37 @@ cities = []*len(coords);
 lat = coords[0,:];
 lon = coords[1,:];
 
-x, y = m(lon, lat)
-plt.plot(x, y, 'ro', markersize=2)
+# edges will be a set of edge tuples, weighted by the third column
+# edges are denoted by node id (linear from 0..2500)
+edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
 
-# for each city,
-#for city, xc, yc in zip(cities, x, y):
-	# draw the city name in a yellow (shaded) box
-	#plt.text(xc+250000, yc-150000, "bird", bbox=dict(facecolor='yellow', alpha=0.5))
+# get coordinates of each edge
+edge_lats_src = np.array([lat[i] for i in edges[:,0]],ndmin=2);
+edge_lons_src = np.array([lon[i] for i in edges[:,0]],ndmin=2);
+edge_lats_trg = np.array([lat[i] for i in edges[:,1]],ndmin=2);
+edge_lons_trg = np.array([lon[i] for i in edges[:,1]],ndmin=2);
+
+weights = 3*edges[:,2].T;
+colors = weights;
+max_weight = weights.max();
+colors = 1 - (colors/max_weight); #normalize to 0..1 and reverse
+sx, sy = m(edge_lons_src, edge_lats_src);
+tx, ty = m(edge_lons_trg, edge_lats_trg);
+
+# points = np.array([x, y]).T.reshape(-1, 1, 2)
+x, y = m(lon, lat);
+plt.plot(x, y, 'ro', markersize=2)
+# segments = np.concatenate([np.array(edge_lats,ndmin=2), np.array(edge_lons,ndmin=2)], axis=0)
+# lc = LineCollection(segments, linewidths=weights,colors='blue')
+# m.add_collection(lc)
+# plt.plot([70, 70], [100, 250], 'k-', lw=2)
+for i in range(len(weights)-1):
+    plt.plot([sx[0][i], tx[0][i]], [sy[0][i], ty[0][i]], linewidth=weights[i], color=my_color(colors[i]));
+
+
+# #for each city,
+# for city, xc, yc in zip(cities, x, y):
+# 	draw the city name in a yellow (shaded) box
+# 	plt.text(xc+250000, yc-150000, "bird", bbox=dict(facecolor='yellow', alpha=0.5))
 
 plt.show()
