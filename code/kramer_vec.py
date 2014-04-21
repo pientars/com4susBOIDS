521509effdd50ef2f748d7704ba1eba6c271195d
R. P <robert.s.pienta@gmail.com>
2014-04-21 04:17:15 -0400

    lol

--- code/kramer_vec.py
+++ code/kramer_vec.py
@@ -2,7 +2,7 @@ import numpy as np
 import time
 #import matplotlib.pyplot as plt
 from math import exp, log, floor, fabs, pi, sqrt, pow
-
+# from plot_bird.py import PlotBirdGraph
 
 def LagCorrelation(v1, v2, lag):
 	# cc = np.correlate(v1,np.hstack([np.zeros(lag), v2[lag:]]));
@@ -17,7 +17,7 @@ def LagCorrelationRange(v1, v2, start, stop):
 	results = np.zeros(stop-start+1);
 	for lag in range(start, stop):
 		results[lag] = LagCorrelation(v1, v2, lag);
-	print results
+	# print results
 
 def FisherTransform(C):
 	one_vec = np.ones(C.shape);
@@ -77,6 +77,18 @@ def FindZeroTimeSeries(data):
 			bad_inds.append(row);
 	return bad_inds
 
+def GetStdWithoutDiag(mat):
+	[m,n] = mat.shape
+	temp = np.zeros(m*n-m)
+	offset = 0;
+	for i in range(0,m):
+		for j in range(0,n):
+			if(i == j):
+				offset+=1
+				continue
+			temp[i*n +j-offset] = mat[i,j] 
+	return np.std(temp)
+
 
 def GetValidEdges(data, zero_inds):
 	t_range = 8;
@@ -97,7 +109,7 @@ def GetValidEdges(data, zero_inds):
 			neigh = Get8Neighbors(map_m, map_n, i)
 			# print neigh
 			for j in neigh:
-				if j%map_n == i//map_n: continue;
+				# if j%map_n == i//map_n: continue;
 				if j in zero_inds: continue;
 				Corrs[i//map_m,j%map_m,t] = FisherTransform(LagCorrelation(data[i,:], data[j,:], t));
 				# Corrs[i,j,t] = FisherTransform(LagCorrelation(data[i,:], data[j,:], t));
@@ -108,8 +120,11 @@ def GetValidEdges(data, zero_inds):
 
 	max_corrs = np.zeros(t_range*num_res_per_lag)
 	max_locs = np.zeros(t_range*num_res_per_lag)				
+	std_by_t = np.zeros(t_range)				
 	# get the max locations and values
 	for t in range(0, t_range):
+		# std_by_t[t] = np.std(Corrs[:,:,t])
+		std_by_t[t] =  GetStdWithoutDiag( Corrs[:,:,t] )
 		for i in range(0, num_res_per_lag):
 			max_locs[t*num_res_per_lag+i] = np.argmax(np.fabs(Corrs[:,:,t]))
 			max_corrs[t*num_res_per_lag+i] = np.max(np.fabs(Corrs[:,:,t])) 
@@ -118,29 +133,28 @@ def GetValidEdges(data, zero_inds):
 			new_j = int(max_locs[t*num_res_per_lag+i]%map_n)
 			Corrs[new_i, new_j, t] = -1;
 	
-	corr_std = np.std(max_corrs)
+	
 	p_val = []
-
+	print(std_by_t)
 	# print "FUCKFUCKFUCKFUCKFUCK"
 	# print max_corrs
 	# print max_locs
 	#Take all the edges with statistically significant edge chance
-	for c in max_corrs:
-		p_val.append( 1.0-ProbZ(c/corr_std, tn-t+1));
+	for c in range(0, len(max_corrs)):
+		p_val.append( 1.0-ProbZ(max_corrs[c]/std_by_t[c//num_res_per_lag], tn));
 
 	# p_val = map( (lambda x: 1.0-ProbZ(x, tn)), max_corrs/corr_std)
 	p_val_w_ind = []
 	for i in range(0, len(p_val)):
 		# make a tuple with (flat loc    pval      tval )
 		p_val_w_ind.append((max_locs[i], p_val[i], i//num_res_per_lag))
-		print('edge: {:d}--->{:d}, p-val={:f}, t={:d} \n'.format(int(max_locs[i]//map_n), int(max_locs[i]%map_n), p_val[i], i//num_res_per_lag));
+		print('edge: {:d}--->{:d}, p-val={:f}, t={:d}'.format(int(max_locs[i]//map_n), int(max_locs[i]%map_n), p_val[i], i//num_res_per_lag));
 	sorted_p_val = sorted(p_val_w_ind, key=lambda tup: tup[1])
 	# print "\nSorted P vals:"
 	# print sorted_p_val	
 
-	
 
-	q = 0.25;
+	q = 0.15;
 	k = FDRController(sorted_p_val, q)
 	edge_list = []
 	# print str(len(sorted_p_val)) + "VS" + str(k)
@@ -149,9 +163,9 @@ def GetValidEdges(data, zero_inds):
 		# print('edge: {:d}--->{:d}, p-val={:f}  t={:d}  '.format(int(floor(max_locs[sorted_p_val[i][0]]/n)), max_locs[sorted_p_val[i][0]]%n, sorted_p_val[i][1], sorted_p_val[i][0]))	
 	
 	for edge in edge_list:
-		print edge
+		print(edge)
 	# print max_locs
-	print '\n'
+	print('\n')
 	# print max_corrs
 	return 0
 
@@ -163,40 +177,13 @@ def PinkNoise(alpha, m, tn):
 	funky = np.vectorize(lambda x: 1/pow(x, alpha))
 	return funky(pdata)
 
-
-
-# def RunPinkNoiseTest():
-# 	data = PinkNoise(.33, 2500, 20)
-# 	data[1, :] *= .2;
-# 	data[1, :] += data[2,:]*.5
-# 	data[3, :] += data[4,:]*.5
-# 	[n,tn] = np.shape(data)
-# 	corr_list = []
-# 	max_corrs = np.zeros(int(floor(tn/2)-1))
-# 	max_locs = []
-# 	for t in range(0,int(floor(tn/2))-1):
-# 		corr_list.append(CAtLagT(data, t))
-# 		max_locs.append(np.argmax(np.fabs(corr_list[t])))
-# 		max_corrs[t] = np.max(np.fabs(corr_list[t]))
-# 	corr_std = np.std(max_corrs)
-# 	p_val_w_ind = [];
-# 	#Take all the edges with statistically significant edge chance
-# 	p_val = map( (lambda x: 1.0-ProbZ(x, tn)), max_corrs/corr_std)
-# 	for i in range(0, len(p_val)):
-# 		p_val_w_ind.append((i, p_val[i]))
-# 	sorted_p_val = sorted(p_val_w_ind, key=lambda tup: tup[1])
-# 	q = 0.15;
-# 	k = FDRController(sorted_p_val, q)
-# 	# for i in range(0, k+1):
-# 		# print('edge: {:d}--->{:d}, p-val={:f}  t={:d}  '.format(int(floor(max_locs[sorted_p_val[i][0]]/n)), max_locs[sorted_p_val[i][0]]%n, sorted_p_val[i][1], sorted_p_val[i][0]))	
-
 # for t  in range(20, 2000, 100):
-data = PinkNoise(.33, 9, 10)
-data[1, :] *= .1;
-data[1, :] += data[2,:]*.5
-data[3, :] += data[4,:]*.5
+# data = PinkNoise(.33, 9, 10)
+# data[1, :] *= .1;
+# data[1, :] += data[2,:]*.5
+# data[3, :] += data[4,:]*.5
 
-zero_inds = []
+# zero_inds = []
 	
 # 	# v1 = np.array([1., 2., 3., 4., 5., 6., 7.])
 # 	# v2 = np.array([1., 1., 1., 4., 5., 6., 7.])
@@ -208,4 +195,4 @@ zero_inds = FindZeroTimeSeries(ts);
 
 a = time.clock()
 GetValidEdges(ts, zero_inds)
-print time.clock() - a
+print(time.clock() - a)
--- code/plot_bird.py
+++ code/plot_bird.py
@@ -8,50 +8,52 @@ def my_color(val):
     """There're better ways to generate unique colors, but this isn't awful."""
     return plt.cm.RdBu(val)
 
-data = np.genfromtxt('bird_coords.csv', delimiter=',');
-coords = data[:,3:5].transpose();
+def PlotBirdGraph(edges):
+	data = np.genfromtxt('bird_coords.csv', delimiter=',');
+	coords = data[:,3:5].transpose();
 
-# Lambert Conformal map of USA lower 48 states
-m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64,
-urcrnrlat=49, projection='lcc', lat_1=33, lat_2=45,
-lon_0=-95, resolution='h', area_thresh=10000)
+	# Lambert Conformal map of USA lower 48 states
+	m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64,
+	urcrnrlat=49, projection='lcc', lat_1=33, lat_2=45,
+	lon_0=-95, resolution='h', area_thresh=10000)
 
-m.drawcoastlines()
-m.drawcountries(linewidth=2)
-m.drawstates()
+	m.drawcoastlines()
+	m.drawcountries(linewidth=2)
+	m.drawstates()
 
-m.drawmapboundary(fill_color='aqua')
-m.fillcontinents(color='coral',lake_color='aqua')
-m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
-m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
+	m.drawmapboundary(fill_color='aqua')
+	m.fillcontinents(color='coral',lake_color='aqua')
+	m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
+	m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
 
-#cities = []*len(coords);
-lat = coords[0,:];
-lon = coords[1,:];
+	#cities = []*len(coords);
+	lat = coords[0,:];
+	lon = coords[1,:];
 
-# edges will be a set of edge tuples, weighted by the third column
-# edges are denoted by node id (linear from 0..2500)
-edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
+	# edges will be a set of edge tuples, weighted by the third column
+	# edges are denoted by node id (linear from 0..2500)
+	if len(edges == 0):
+		edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
 
-# get coordinates of each edge
-edge_lats_src = np.array([lat[i] for i in edges[:,0]],ndmin=2);
-edge_lons_src = np.array([lon[i] for i in edges[:,0]],ndmin=2);
-edge_lats_trg = np.array([lat[i] for i in edges[:,1]],ndmin=2);
-edge_lons_trg = np.array([lon[i] for i in edges[:,1]],ndmin=2);
+	# get coordinates of each edge
+	edge_lats_src = np.array([lat[i] for i in edges[:,0]],ndmin=2);
+	edge_lons_src = np.array([lon[i] for i in edges[:,0]],ndmin=2);
+	edge_lats_trg = np.array([lat[i] for i in edges[:,1]],ndmin=2);
+	edge_lons_trg = np.array([lon[i] for i in edges[:,1]],ndmin=2);
 
-weights = 3*edges[:,2].T;
-colors = weights;
-max_weight = weights.max();
-colors = 1 - (colors/max_weight); #normalize to 0..1 and reverse
+	weights = 3*edges[:,2].T;
+	colors = weights;
+	max_weight = weights.max();
+	colors = 1 - (colors/max_weight); #normalize to 0..1 and reverse
 
-sx, sy = m(edge_lons_src, edge_lats_src);
-tx, ty = m(edge_lons_trg, edge_lats_trg);
+	sx, sy = m(edge_lons_src, edge_lats_src);
+	tx, ty = m(edge_lons_trg, edge_lats_trg);
 
-x, y = m(lon, lat);
-plt.plot(x, y, 'ro', markersize=2)
+	x, y = m(lon, lat);
+	plt.plot(x, y, 'ro', markersize=2)
 
-# use line collections if this slows down
-for i in range(len(weights)-1):
-    plt.plot([sx[0][i], tx[0][i]], [sy[0][i], ty[0][i]], linewidth=weights[i], color=my_color(colors[i]));
+	# use line collections if this slows down
+	for i in range(len(weights)-1):
+	    plt.plot([sx[0][i], tx[0][i]], [sy[0][i], ty[0][i]], linewidth=weights[i], color=my_color(colors[i]));
 
-plt.show()
+	plt.show()
