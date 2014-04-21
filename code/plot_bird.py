bdc020c265951cf175c698118592df3bb0e8714c
Casey Battaglino <cjbattagl@gmail.com>
2014-04-21 19:16:53 -0400

    uhh should work?

--- code/plot_bird.py
+++ code/plot_bird.py
@@ -21,35 +21,36 @@ def my_color(val):
 def get_edges_from_period(period):
 	data = np.genfromtxt('map_vals.csv', delimiter=',');
 	edges = data[data[:, 0] == period];
-	edges = edges[edges[:, 1] >= 2];
+	edges = edges[edges[:, 1] >= 1];
 	edges = edges[:,2::];
 	return edges;
 
 data = np.genfromtxt('bird_coords.csv', delimiter=',');
 coords = data[:,3:5].transpose();
 
-# Lambert Conformal map of USA lower 48 states
-m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64,
-urcrnrlat=49, projection='lcc', lat_1=33, lat_2=45,
-lon_0=-95, resolution='c', area_thresh=10000)
-
-m.drawcoastlines()
-m.drawcountries(linewidth=2)
-m.drawstates()
-
-m.drawmapboundary(fill_color='aqua')
-m.fillcontinents(color='coral',lake_color='aqua')
-m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
-m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
-
-#cities = []*len(coords);
-lat = coords[0,:];
-lon = coords[1,:];
-
-# edges will be a set of edge tuples, weighted by the third column
-# edges are denoted by node id (linear from 0..2500)
-#edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
 for time in range(8):
+	# Lambert Conformal map of USA lower 48 states
+	m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64,
+	urcrnrlat=49, projection='lcc', lat_1=33, lat_2=45,
+	lon_0=-95, resolution='c', area_thresh=10000)
+
+	m.drawcoastlines()
+	m.drawcountries(linewidth=2)
+	m.drawstates()
+
+	m.drawmapboundary(fill_color='aqua')
+	m.fillcontinents(color='coral',lake_color='aqua')
+	m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
+	m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
+
+	#cities = []*len(coords);
+	lat = coords[0,:];
+	lon = coords[1,:];
+
+	# edges will be a set of edge tuples, weighted by the third column
+	# edges are denoted by node id (linear from 0..2500)
+	#edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
+
 	window = time*5;
 	edges = get_edges_from_period(window);
 
@@ -68,7 +69,7 @@ for time in range(8):
 	tx, ty = m(edge_lons_trg, edge_lats_trg);
 
 	x, y = m(lon, lat);
-	plt.plot(x, y, 'ro', markersize=2)
+	#plt.plot(x, y, 'ro', markersize=2)
 
 	# use line collections if this slows down
 	for i in range(len(weights)-1):
@@ -76,4 +77,5 @@ for time in range(8):
 			plt.plot([sx[0][i], tx[0][i]], [sy[0][i], ty[0][i]], linewidth=weights[i], color=my_color(colors[i]));
 
 	plt.savefig('bird' + str(window) + '.png')
+	plt.hold(False);
 	#plt.show()
