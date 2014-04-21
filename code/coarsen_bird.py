ad0c87f771a0af6578c1dca1a58dda5c1b4b28ea
Casey Battaglino <cjbattagl@gmail.com>
2014-04-20 20:51:08 -0400

    plot and coarsen now outputs two (2) files

--- code/coarsen_bird.py
+++ code/coarsen_bird.py
@@ -1,6 +1,12 @@
 import numpy as np
 import csv
 
+def get_horz_node(longitude, min_long, long_dim):
+	return int((longitude - min_long) // long_dim);
+
+def get_vert_node(latitude, min_lat, lat_dim):
+	return int((latitude - min_lat) // lat_dim);
+
 bird_locs = np.genfromtxt('tree_swallow_locs.csv', delimiter=',');
 bird_locs = bird_locs[1::,1::];
 
@@ -32,8 +38,8 @@ for line in reader:
 		continue
 	coord_id = int(line[0]) - 1;
 	ts = [float(val) for val in line[1::]];
-	h = int(get_horz_node(bird_locs[coord_id,1],min_long,long_dim));
-	v = int(get_vert_node(bird_locs[coord_id,0],min_lat,lat_dim));
+	h = get_horz_node(bird_locs[coord_id,1],min_long,long_dim);
+	v = get_vert_node(bird_locs[coord_id,0],min_lat,lat_dim);
 	graph[v][h] = map(lambda x: x[0] + x[1], zip(graph[v][h], ts))
 	#graph[v][h] = [x+y for x,y in zip(graph[v][h], ts)];
 	num_ts[v][h] += 1;
@@ -42,17 +48,22 @@ for i,j in np.ndindex((num_div, num_div)):
 	if (num_ts[i][j] >= 1):
 		graph[i][j] = [x / num_ts[i][j] for x in graph[i][j]];
 
-with open('bird_ts.csv', 'wb') as csvfile:
-	graphwriter = csv.writer(csvfile, delimiter=',');
+with open('bird_coords.csv', 'wb') as csvfile:
+	graphwriter = csv.writer(csvfile, delimiter=',', dialect='excel');
 	for i,j in np.ndindex((num_div, num_div)):
 		# place the node coordinates in the center of the bounding box
 		node_lat = graph_lats[i] + (lat_dim / 2);
 		node_long = graph_longs[j] + (long_dim / 2);
-		data_to_write = [num_ts[i][j], i, j, node_lat, node_long, graph[i][j]];
+
+		data_to_write = [num_ts[i][j], i, j, node_lat, node_long];#, graph[i][j]];
 		graphwriter.writerow(data_to_write);
 
-def get_horz_node(longitude, min_long, long_dim):
-	return int((longitude - min_long) // long_dim);
+with open('bird_timeseries.csv', 'wb') as csvfile:
+	graphwriter = csv.writer(csvfile, delimiter=',', dialect='excel');
+	for i,j in np.ndindex((num_div, num_div)):
+		# place the node coordinates in the center of the bounding box
+		node_lat = graph_lats[i] + (lat_dim / 2);
+		node_long = graph_longs[j] + (long_dim / 2);
 
-def get_vert_node(latitude, min_lat, lat_dim):
-	return int((latitude - min_lat) // lat_dim);
+		data_to_write = [graph[i][j]];
+		graphwriter.writerow(data_to_write);
