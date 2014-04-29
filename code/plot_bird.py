f0686090b740e71bfbb2e6a63feabbe44c51c499
Casey Battaglino <cjbattagl@gmail.com>
2014-04-29 19:10:29 -0400

    drawheatmap

--- code/plot_bird.py
+++ code/plot_bird.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from matplotlib.collections import LineCollection
+from matplotlib.mlab import griddata
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import sys
@@ -28,60 +30,74 @@ def get_edges_from_period(period):
 
 
 
-def plot_birds_okay(time):
-	data = np.genfromtxt('bird_coords.csv', delimiter=',');
-	coords = data[:,3:5].transpose();
+#def plot_birds_okay(time):
+time = 20
+data = np.genfromtxt('bird_coords.csv', delimiter=',');
+coords = data[:,3:5].transpose();
 
-	# # Lambert Conformal map of USA lower 48 states
-	m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64,
-	urcrnrlat=49, projection='lcc', lat_1=33, lat_2=45,
-	lon_0=-95, resolution='c', area_thresh=10000)
+# # Lambert Conformal map of USA lower 48 states
+m = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64,
+urcrnrlat=49, projection='lcc', lat_1=33, lat_2=45,
+lon_0=-95, resolution='c', area_thresh=10000)
 
-	m.drawcoastlines()
-	# m.drawcountries(linewidth=1.5)
-	# m.drawstates(color='.5')
+m.drawcoastlines()
+# m.drawcountries(linewidth=1.5)
+# m.drawstates(color='.5')
 
-	m.drawmapboundary(fill_color='.9')
-	m.fillcontinents(color='.6',lake_color='.9')
-	m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
-	m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
+m.drawmapboundary(fill_color='.9')
+#m.fillcontinents(color='.6',lake_color='.9')
+m.drawparallels(np.arange(25,65,20),labels=[1,0,0,0])
+m.drawmeridians(np.arange(-120,-40,20),labels=[0,0,0,1])
 
-	#cities = []*len(coords);
-	lat = coords[0,:];
-	lon = coords[1,:];
+#cities = []*len(coords);
+lat = coords[0,:];
+lon = coords[1,:];
 
-	# edges will be a set of edge tuples, weighted by the third column
-	# edges are denoted by node id (linear from 0..2500)
-	#edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
-	print(sys.argv[1])
-	time = int(sys.argv[1]);
-	window = time*2;
-	edges = get_edges_from_period(window);
+# edges will be a set of edge tuples, weighted by the third column
+# edges are denoted by node id (linear from 0..2500)
+#edges = np.array([[20, 25, 0.6], [1300,2000,0.4], [1200,2134,0.5], [300,301,1.5], [200,201,1.2], [600,700,1.8], [25,26,0.5], [27,29,0.5], [1400,1405,1.2], [407,409,1.5]]);
+#print(sys.argv[1])
+#time = int(sys.argv[1]);
+window = time*2;
+edges = get_edges_from_period(window);
 
-	# get coordinates of each edge
-	edge_lats_src = np.array([lat[i] for i in edges[:,0]],ndmin=2);
-	edge_lons_src = np.array([lon[i] for i in edges[:,0]],ndmin=2);
-	edge_lats_trg = np.array([lat[i] for i in edges[:,1]],ndmin=2);
-	edge_lons_trg = np.array([lon[i] for i in edges[:,1]],ndmin=2);
+# get coordinates of each edge
+edge_lats_src = np.array([lat[i] for i in edges[:,0]],ndmin=2);
+edge_lons_src = np.array([lon[i] for i in edges[:,0]],ndmin=2);
+edge_lats_trg = np.array([lat[i] for i in edges[:,1]],ndmin=2);
+edge_lons_trg = np.array([lon[i] for i in edges[:,1]],ndmin=2);
 
-	weights = edges[:,2].T;
-	colors = weights;
-	alphas = edges[:,3].T;
-	max_weight = weights.max();
-	colors = 1 - (colors/max_weight); #normalize to 0..1 and reverse
+weights = edges[:,2].T;
+colors = weights;
+alphas = edges[:,3].T;
+max_weight = weights.max();
+colors = 1 - (colors/max_weight); #normalize to 0..1 and reverse
 
-	sx, sy = m(edge_lons_src, edge_lats_src);
-	tx, ty = m(edge_lons_trg, edge_lats_trg);
+sx, sy = m(edge_lons_src, edge_lats_src);
+tx, ty = m(edge_lons_trg, edge_lats_trg);
 
-	x, y = m(lon, lat);
-	#plt.plot(x, y, 'ro', markersize=2)
+x, y = m(lon, lat);
+#plt.plot(x, y, 'ro', markersize=2)
 
-	#use line collections if this slows down
-	for i in range(len(weights)-1):
-		if (weights[i] > 0.1):
-			plt.plot([sx[0][i], tx[0][i]], [sy[0][i], ty[0][i]], linewidth=.5, alpha=alphas[i], color=my_color(colors[i]));
+#use line collections if this slows down
+for i in range(len(weights)-1):
+	if (weights[i] > 0.1):
+		plt.plot([sx[0][i], tx[0][i]], [sy[0][i], ty[0][i]], linewidth=.5, alpha=alphas[i], color=my_color(colors[i]));
 
-	plt.savefig('bird' + str(window) + '.png')
-	# #plt.show()
+ts = np.genfromtxt('bird_timeseries.csv', delimiter=',');
+#clevs = [0,0.02,0.04,0.06,0.08,0.1,0.12,0.14,0.16,0.18,0.2,0.22,0.24,0.26,0.28,0.3,1]
+tsdata = ts[:,window].T;
 
-plot_birds_okay(0);
+min_lon = lat.min(axis=0); max_lat = lat.max(axis=0);
+min_lon = lon.min(axis=0); max_lon = lon.max(axis=0);
+xi = np.linspace(min_lon,max_lon,50)
+yi = np.linspace(min_lat,max_lat,50)
+lon2, lat2 = np.meshgrid(xi,yi)
+xo, yo = m(lon2, lat2)
+#zi = griddata(x,y,tsdata,xi,yi,interp='nn')
+vals = tsdata.reshape([50,50]);
+cs = m.contourf(xo,yo,vals,cmap=plt.cm.rainbow)
+plt.savefig('bird' + str(window) + '.png')
+plt.show()
+
+#plot_birds_okay(0);
